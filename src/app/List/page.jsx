569d5a314eb69a1fc1816118f193c50435d1304e
Aman Kumar <amanksh05@gmail.com>
2024-10-01 02:59:53 +0530

    layout changes

--- src/app/List/page.jsx
+++ src/app/List/page.jsx
@@ -1,17 +1,29 @@
 import Card from '@/components/Card'
-import Link from 'next/link'
 import React from 'react'
 
 function page() {
-  return (
-    <div>
-      <Card name={"Dijkstra"}desc={"lorem ipsum "}/>
-      {/* <Link href={"/Dijkstra"}>Dijkstra</Link>
-      <Link href={"/least-recently-used"}>LRU</Link> */}
+  const list = [
+    {
+      "name": "Dijkstra",
+      "desc": "Shortest path finder from source to node",
+      "link": "/Dijkstra"
+    },
+    {
+      "name": "Least recently used",
+      "desc": "Operating system concept",
+      "link": "/least-recently-used"
+    },
+  ]
 
+  return (
+    <div className="grid grid-flow-col gap-5 mt-10">
+      {
+        list.map((item, key) => (
+          <Card key={key} name={item.name} desc={item.desc} link={item.link} />
+        ))
+      }
     </div>
-    
   )
 }
 
-export default page
+export default page;
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 import { Button } from './ui/button'
 import Link from 'next/link';
 
-function Card({ name, desc }) {
+function Card({ name, desc, link }) {
     return (
         <div
             className='flex flex-col items-start justify-between gap-4 w-96 border border-gray-300 p-6 rounded-lg shadow-lg hover:shadow-2xl transition-shadow duration-300 ease-in-out min-h-48 max-h-48 relative overflow-hidden'
@@ -22,10 +22,10 @@ function Card({ name, desc }) {
             </div>
 
             <div className='flex gap-4 relative z-10'>
-                <Link href={"/Dijkstra"}>
+                <Link href={"/null"}>
                     <Button className="bg-white border border-black text-black hover:bg-gray-100 transition duration-200">About</Button>
                 </Link>
-                <Link href={"/Dijkstra"}>
+                <Link href={link}>
                     <Button className="bg-black text-white border border-gray-800 hover:bg-gray-800 transition duration-200">View</Button>
                 </Link>
             </div>
