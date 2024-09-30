b2de372431dafe788eb2d0bb75d88741691b3074
Aman Kumar <amanksh05@gmail.com>
2024-10-01 02:50:34 +0530

    btn modification

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,10 +5,10 @@ import Link from 'next/link';
 
 function Card({ name, desc }) {
     return (
-        <div 
-            className='flex flex-col items-start justify-between gap-4 w-96 border border-gray-300 p-6 rounded-lg shadow-lg hover:shadow-2xl transition-shadow duration-300 ease-in-out min-h-48 max-h-48 relative overflow-hidden' 
+        <div
+            className='flex flex-col items-start justify-between gap-4 w-96 border border-gray-300 p-6 rounded-lg shadow-lg hover:shadow-2xl transition-shadow duration-300 ease-in-out min-h-48 max-h-48 relative overflow-hidden'
             style={{
-                backgroundImage: `url('https://www.toptal.com/designers/subtlepatterns/uploads/dot-grid.png')`, 
+                backgroundImage: `url('https://www.toptal.com/designers/subtlepatterns/uploads/dot-grid.png')`,
                 backgroundSize: 'cover'
             }}
         >
@@ -22,9 +22,11 @@ function Card({ name, desc }) {
             </div>
 
             <div className='flex gap-4 relative z-10'>
-                <Button className = "bg-white border border-black text-black hover:bg-gray-100 transition duration-200">About</Button>
                 <Link href={"/Dijkstra"}>
-                <Button className="bg-black text-white border border-gray-800 hover:bg-gray-800 transition duration-200">View</Button>
+                    <Button className="bg-white border border-black text-black hover:bg-gray-100 transition duration-200">About</Button>
+                </Link>
+                <Link href={"/Dijkstra"}>
+                    <Button className="bg-black text-white border border-gray-800 hover:bg-gray-800 transition duration-200">View</Button>
                 </Link>
             </div>
         </div>
